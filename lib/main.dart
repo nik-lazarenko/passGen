36d6f7b3e8a6c41d4ab93634c8a0a3701120e012
nikitalazarenko <nikitalazarenko@gmail.com>
2024-03-04 18:37:26 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,34 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main(List<String> arguments) {
-  String password = generatePassword();
+  try {
+    final length = int.tryParse(arguments[0]) ?? 0;
+    String password = generatePassword(length);
 
-  print(
-    'Сгенерированный пароль: '
-    '$password',
-  );
+    print(
+      'Сгенерированный пароль: '
+          '$password',
+    );
+  }
+  catch (e) {
+    print('Нет аргумента');
+  }
 }
 
+String generatePassword(int length) {
+  const charset = 'abcdefghijklmnopqrstuvwyz'
+      'ABCDEFGHIJKLMNOPQRSTUVWYZ'
+      '0123456789';
+
+
+  var random = Random.secure();
+
+  return String.fromCharCodes(
+    List.generate(
+      length,
+      (_) => charset.codeUnitAt(
+        random.nextInt(charset.length),
+      ),
+    ),
+  );
+}
